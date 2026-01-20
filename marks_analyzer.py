c17482f4c2dda296ea1d3a509ef1880e3bf0e1cb
Ummeaisha <ummeaisha76@gmail.com>
2026-01-21 02:00:50 +0500

    Update marks_analyzer.py

--- marks_analyzer.py
+++ marks_analyzer.py
@@ -5,6 +5,7 @@ for i in range(1,6):
 
 total=sum(marks)
 average=total/len(marks)
+print("\n--- Student Marks Summary ---")
 print("Total Marks of Student",total)
 print("Average of Marks:", average)
 print("Highest Marks", max(marks))
