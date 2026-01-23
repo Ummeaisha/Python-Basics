a60593e2316be71aa64194ed4c4d2af4b13fc4ac
Ummeaisha <ummeaisha76@gmail.com>
2026-01-24 04:17:24 +0500

    Update marks_analyzer.py

--- marks_analyzer.py
+++ marks_analyzer.py
@@ -1,15 +1,27 @@
-marks=[]
-for i in range(1,6):
-   mark=int(input(f"Enter the marks {i}: "))
-   marks.append(mark)
+marks = []
 
-total=sum(marks)
-average=total/len(marks)
-print("\n--- Student Marks Summary ---")
-print("Total Marks of Student",total)
-print("Average of Marks:", average)
-print("Highest Marks", max(marks))
-print("lowest marks ", min(marks))
+while len(marks) < 5:
+    mark = int(input(f"Enter marks for subject {len(marks) + 1}: "))
 
+    if mark < 0 or mark > 100:
+        print("Invalid input! Marks must be between 0 and 100.")
+        continue
 
+    marks.append(mark)
 
+avg = sum(marks) / len(marks)
+
+print("\n--- Result ---")
+
+if avg >= 95:
+    print("Your Grade = O")
+elif avg >= 90:
+    print("Your Grade = A")
+elif avg >= 80:
+    print("Your Grade = B")
+elif avg >= 70:
+    print("Your Grade = C")
+elif avg >= 60:
+    print("Your Grade = D")
+else:
+    print("Your Grade = Fail")
